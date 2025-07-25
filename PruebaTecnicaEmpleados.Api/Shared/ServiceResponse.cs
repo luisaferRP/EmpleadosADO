e7804fafc8b9af5ea2edbe1c789420dd88e64ddf
luisaferRP <luisaramiresporras103@gmail.com>
2025-07-25 10:37:54 -0500

    feat: is created ServiceResponse

---
+++ PruebaTecnicaEmpleados.Api/Shared/ServiceResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaTecnicaEmpleados.Api.Shared
+{
+    public class ServiceResponse<T>
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+        public T Data { get; set; }
+
+        public static ServiceResponse<T> Ok(T data, string message = "") =>
+            new ServiceResponse<T> { Data = data, Message = message };
+
+        public static ServiceResponse<T> Fail(string message) =>
+            new ServiceResponse<T> { Success = false, Message = message };
+    }
+}
