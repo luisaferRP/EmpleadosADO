00e7f27f538fa2f7a152b81f812b255ecd256bd4
luisaferRP <luisaramiresporras103@gmail.com>
2025-07-25 00:57:27 -0500

    first commit

---
+++ PruebaTecnicaEmpleados.Api/Repository/Implementations/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using PruebaTecnicaEmpleados.Api.Models;
+using PruebaTecnicaEmpleados.Api.Repository.Interface;
+
+namespace PruebaTecnicaEmpleados.Api.Repository.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly string connectionString;
+
+        public UserRepository()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString; ;
+        }
+        public Task AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PruebaTecnicaEmpleados.Api/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PruebaTecnicaEmpleados.Api.Models;
+
+namespace PruebaTecnicaEmpleados.Api.Repository.Interface
+{
+    internal interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
